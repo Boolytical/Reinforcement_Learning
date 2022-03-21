5c58781cd461c0a3981a77abb3daba670cf80eca
Felixkap <kapullafelix@gmail.com>
2022-03-21 21:19:11 +0100

    Neater Script

--- dqn2.py
+++ dqn2.py
@@ -1,31 +1,27 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras.layers import Input, Dense, Activation
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 import gym
 import random
-import time
-from matplotlib import pyplot as plt
 
 
 class DQNAgent:
     def __init__(self, env, policy):
 
-        self.alpha = 0.001  # learning rate
-        self.gamma = 0.99  # discount factor
-        self.epsilon = 1.0  # exploration rate
-        self.epsilon_min = 0.001  # minimum exploration rate
-        self.policy = policy
-        self.replay_buffer = []
-        self.replay_buffer_max_length = 500
-        self.n_actions = env.action_space.n  # soze of action space
-        self.states_size = env.observation_space.shape[0]  # size of observation space
+        self.alpha = 0.001  # Learning rate
+        self.gamma = 0.99  # Discount factor
+        self.epsilon = 1.0  # Exploration rate
+        self.epsilon_min = 0.001  # Minimum exploration rate
+        self.policy = policy # Policy
+        self.replay_buffer = [] # Initialize replay buffer
+        self.replay_buffer_max_length = 500 # Maximum length of replay buffer
+        self.n_actions = env.action_space.n  # Size of action space
+        self.states_size = env.observation_space.shape[0]  # Size of observation space
         self.batch_size = 64  # Batch size
 
-        ### Construct Deep Neural Network
 
+        ### Construct Deep Neural Network
     def _construct_dnn(self, input_size, output_nodes):
         model = Sequential(
             [
@@ -38,17 +34,17 @@ class DQNAgent:
         model.compile(optimizer=Adam(learning_rate=0.1), loss='mse')
         return model
 
-    def select_action(self, model, s, epsilon=0.05, temp=1.0):
+    def select_action(self, model, s, epsilon=0.05):
         if self.policy == 'egreedy':
             # explore action space with probability of epsilon
             if np.random.uniform(0, 1) < epsilon:
                 a = np.random.randint(0, self.n_actions)
-                # greedy policy with probability 1-epsilon
+            # greedy policy with probability 1-epsilon
             else:
-                q_predictions = model.predict(s.reshape((1, 4)))[
-                    0]  # given a state compute q values for each action (output of Q_network)
+                q_predictions = model.predict(s.reshape((1, 4)))[0]  # given a state compute q-values for each action (output of Q_network)
                 a = np.argmax(q_predictions)  # choose action that correspond to highest estimated q-value
             return a
+        #### Add other behaviour policies
 
     def epsilon_decay(self, max_episode_length):  # when episode ends, update exploration rate
         epsilon_delta = (self.epsilon - self.epsilon_min) / max_episode_length
@@ -62,6 +58,7 @@ class DQNAgent:
             'state_next': s_next,
             'done': done
         })
+        # If replay buffer is larger then pre-defined maximum buffer length, remove oldest experience
         if len(self.replay_buffer) > self.replay_buffer_max_length:
             self.replay_buffer.pop(0)
 
@@ -72,13 +69,13 @@ class DQNAgent:
         actions, rewards, dones = [], [], []
 
         for cnt, experience in enumerate(batch):
-            states[cnt, :] = experience['state']
-            states_next[cnt, :] = experience['state_next']
-            actions.append(experience['action'])
-            rewards.append(experience['reward'])
+            states[cnt, :] = experience['state'] # Collect states of all experiences from batch
+            states_next[cnt, :] = experience['state_next'] # Collect new states of all experiences from batch
+            actions.append(experience['action']) # Collect actions of all experiences from batch
+            rewards.append(experience['reward']) # Collect rewards of all experiences from batch
             dones.append(experience['done'])
 
-        output = model.predict(states)  # Dim: batch_size x 2(actions)
+        output = model.predict(states)  # Dim: batch_size x 2(actions) --> Primary network
         target = model.predict(states_next)  # Dim: batch_size x 2(actions)
 
         for i in range(self.batch_size):
@@ -104,9 +101,8 @@ def DQN(n_episodes, max_episode_length, policy):
         for t in range(max_episode_length):
             a = agent.select_action(q_net, s)
             s_next, r, done, _ = env_cart.step(a)  # State: Box(4), Reward: float, done: Boolean
-            rewards_episode += r
-            agent.collect_episode(s, a, r, s_next,
-                                  done)  # Collect episode: Store experience of each time step within an episode
+            rewards_episode += r # Compute reward of the episode
+            agent.collect_episode(s, a, r, s_next,done)  # Collect episode: Store experience of each time step within an episode
             t_step_total += 1  # count total number of time steps
             s = s_next
 
@@ -114,11 +110,11 @@ def DQN(n_episodes, max_episode_length, policy):
                 break
 
         print(f'Episode: {episode + 1} with exploration rate {agent.epsilon} had reward of {rewards_episode}')
-        rewards.append(rewards_episode)  # collect rewards of each episodes --> corresponds to length of episode: t
+        rewards.append(rewards_episode)  # collect rewards of each episodes
         if agent.epsilon > agent.epsilon_min:
-            agent.epsilon_decay(max_episode_length)
+            agent.epsilon_decay(max_episode_length) # Decaying Exploration Rate
 
-        # after finishing an episode, train the neural network (Q-value function approx.)
+        # After finishing an episode, train the Q-network (Q-value function approx.)
         if t_step_total >= agent.batch_size:
             agent.train_Qnetwork(q_net)
 
@@ -131,8 +127,6 @@ def test():
 
     # Exploration
     policy = 'egreedy'  # 'egreedy' or 'softmax'
-    epsilon = 0.1
-    temp = 1.0
 
     results = DQN(n_episodes, max_episode_length, policy)
     return results
